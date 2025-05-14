5656fa8b850ea8de72ed334c4aaf6d1f529f5d16
krivipet <181596453+krovinskii@users.noreply.github.com>
2025-05-14 16:31:20 -0500

    and we go back

--- src/components/Card.jsx
+++
@@ -1,9 +0,0 @@
-//Takes in one prop "img" and renders an image with padding
-function Card({ img }) {
-  return (
-    <div className="p-[.5rem] shadow-xl rounded-lg bg-white w-[15rem] h-[20vh]  box-border">
-      <img src={img} alt="Card Img" className="h-full w-full object-cover" />
-    </div>
-  );
-}
-export default Card;
